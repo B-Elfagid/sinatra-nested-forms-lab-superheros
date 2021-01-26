2b6098aa6dc2d5bc030930f05a420002d6180540
B-Elfagid <betty_elfagid@yahoo.co.uk>
2021-01-26 01:15:05 +0000

    commit tree e08bd18e41866b39d615983b3418e9ded8434cec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,5 @@ post '/teams' do
     @heroes = Hero.all
     erb :team
   end
-#post '/teams' do
-   # @team_name = params["team"]["name"]
-    #@team_motto = params["team"]["motto"]
-    #@team_members = params["team"]["members"]
-  
-#end
+
 end 
